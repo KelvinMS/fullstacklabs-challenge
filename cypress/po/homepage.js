89ffd0a6d87342c8fad1524e9ff7d56e0e419281
Kelvin Moreira <ext.kelvin.santos@farfetch.com>
2023-01-24 16:51:02 -0300

    Add new fuctions and elements

--- cypress/po/homepage.js
+++ cypress/po/homepage.js
@@ -2,7 +2,8 @@ class HomePagePO{
 
     button = {
         
-        Submit:"[data-testid=\"btn-submit\"]"
+        Submit:"[data-testid=\"btn-submit\"]",
+        Delete:"[data-testid=\"btn-delete\"]"
     };
     textbox ={
         PetName:"[data-testid=\"pet\"]",
@@ -18,6 +19,24 @@ class HomePagePO{
         Time:':nth-child(4) > span',
         Symptoms:':nth-child(5) > span',
         DynamicAppointmentTitle:'[data-testid="dynamic-title"]',
+    };
+    comboBox = {
+        existingAppointment :'[data-testid="appointment"]'
+    };
+
+    clickSubmit(){
+        cy.get(this.button.Submit).click();
+    }
+    
+
+    fillFormInfo(allFields){
+        cy.get(this.textbox.PetName).type('Shinigami');
+        cy.get(this.textbox.OwnerName).type('Kelvin Moreira');
+        cy.get(this.textbox.Date).type('2023-01-18');
+        cy.get(this.textbox.Time).type("00:23");
+        if(allFields){
+            cy.get(this.textbox.Symptoms).type("Joy");
+        }
     }
 
 };
